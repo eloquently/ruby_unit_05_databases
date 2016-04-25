a58cf632649d9e1683cd2b681331de8eb5a929f1
etmckay1 <ericmckay4@gmail.com>
2016-04-25 06:33:28 +0000

    step 2

---
+++ lib/district_analyzer.rb
@@ -0,0 +1,98 @@
+require 'sqlite3'
+
+class DistrictAnalyzer
+    attr_accessor :path_to_db
+
+    def initialize(path_to_db)
+        self.path_to_db = path_to_db
+    end
+
+    def query(q, results_as_hash=true)
+        db = SQLite3::Database.new(path_to_db)
+        db.results_as_hash = results_as_hash
+        result = db.execute(q)
+        db.close
+        return result
+    end
+
+    # count how many students are in the district
+    # should return an integer
+    def count_students
+        query = <<-SQL
+        SELECT
+            COUNT(*)
+        FROM
+            students;
+        SQL
+        result = query(query, false)
+        return result[0][0]
+    end
+
+    # count how many students are at a particular school
+    # should return an integer
+    def count_students_at_school(school_id)
+    end
+
+    # get a list of all teachers' first and last names that teach at school id
+    # this method should return an array where each element is a hash
+    # containing the first name and last name of the people of interest
+    # e.g. [ { first_name: "Jane", last_name: "Doe" },
+    #        { first_name: "John", last_name: "Smith" }, ... ]
+    def list_teachers(school_id)
+    end
+
+    # get a list of all teachers' first and last names that teach at school id
+    # this method should return an array where each element is a hash
+    # containing the first name and last name of the people of interest
+    # e.g. [ { first_name: "Jane", last_name: "Doe" },
+    #        { first_name: "John", last_name: "Smith" }, ... ]
+    def teacher_info(teacher_id)
+    end
+
+    # get a list of classes offered by a school.
+    # this method should return an array where each element is an array with one
+    # element
+    # e.g. [ ["Spanish"], ["French"], ... ]
+    def class_catalog(school_id)
+    end
+
+     # find the hardest n teachers in the district based on the average test
+    # scores in all of their classes
+    # e.g. [ { first_name: "Jane", last_name: "Doe", avg_test_score: 65 },
+    #        { first_name: "Jane", last_name: "Doe", avg_test_score: 65 }, ... ]
+    def hardest_teachers(n)
+    end
+
+    # return the top n students in the district based on their average test
+    # score this method should return an array where each element is a hash
+    # containing each student's first name, last name, school name, and score
+    # e.g. [ { first_name: "Jane", last_name: "Doe",
+    #          school_name: "Tempe High School", avg_test_score: 99 },
+    #        { first_name: "Cindy", last_name: "Doe",
+    #          school_name: "Tempe Middle School", avg_test_score: 95 }, ... ]
+    def top_students(n)
+    end
+
+    # return the bottom n students in the district based on their average test
+    # score this method should return an array where each element is a hash
+    # containing the first name, last name, and school name of the people
+    # e.g. [ { first_name: "Jane", last_name: "Doe", school_name: "" },
+    #        { first_name: "John", last_name: "Smith", school_name: "" }, ... ]
+    def bottom_students(n)
+    end
+
+    # at the end of the school year a few things need to happen.
+    # 1) All students should be advanced one grade
+    # 2) Students who have graduated from one school should be sent to the next
+    #       school. Students who have graduated from high school should be
+    #       removed from the database.
+    # 3) All data describing which students are in which classes should be
+    #       removed from the database.
+    # 4) All data on tests should be removed from the database.
+    # this method does not need to return anything.
+
+    # write your own tests for this method using SQL queries to ensure
+    # that all the conditions are met.
+    def year_end_updates
+    end
+end
---
+++ utils/generate_seeds.rb
@@ -0,0 +1,100 @@
+file = open('lib/seed_data.sql', 'w')
+
+school_setup = <<-sql
+INSERT INTO schools (id, name, min_grade, max_grade)
+    VALUES (1, 'Phoenix High School', 9, 12);
+
+INSERT INTO schools (id, name, min_grade, max_grade, next_school_id)
+    VALUES (2, 'Phoenix Middle School', 6, 8, 1);
+
+INSERT INTO schools (id, name, min_grade, max_grade, next_school_id)
+    VALUES (3, 'Phoenix Elementary School', 1, 5, 2);
+
+INSERT INTO schools (id, name, min_grade, max_grade)
+    VALUES (4, 'Tempe High School', 9, 12);
+
+INSERT INTO schools (id, name, min_grade, max_grade, next_school_id)
+    VALUES (5, 'Tempe Middle School', 6, 8, 4);
+
+INSERT INTO schools (id, name, min_grade, max_grade, next_school_id)
+    VALUES (6, 'Tempe Elementary School', 1, 5, 5);
+
+sql
+
+file.write(school_setup)
+
+first_names = File.read('first_names.csv').split("\n").map(&:capitalize)
+last_names = File.read('last_names.csv').split("\n").map(&:capitalize)
+
+school_ids = [1,2,3,4,5,6]
+subjects = ["English", "History", "Civics", "Algebra", "Spanish",
+            "French", "German", "Geometry", "Calculus", "Programming",
+            "PE", "Chemistry", "Physics", "Biology"]
+
+teachers_per_school = 10
+students_per_school = 200
+classes_per_teacher = 3
+classes_per_student = 3
+tests_per_student_per_class = 2
+
+teacher_data = ""
+class_data = ""
+student_data = ""
+join_data = ""
+test_data =""
+
+school_ids.each do |s|
+    teachers_per_school.times do |i|
+        teacher_id = (s-1)*teachers_per_school+(i+1)
+        f = first_names.sample
+        l = last_names.sample
+
+        teacher_data += "INSERT INTO teachers (id, first_name, last_name, school_id) VALUES (#{teacher_id}, \"#{f}\", \"#{l}\", #{s});\n"
+
+        classes_per_teacher.times do |j|
+            class_id = (teacher_id-1)*classes_per_teacher+(j+1)
+            n = subjects.sample
+            class_data += "INSERT INTO classes (id, name, teacher_id) VALUES (#{class_id}, \"#{n}\", #{teacher_id});\n"
+        end
+    end
+
+    students_per_school.times do |i|
+        student_id = (s-1)*students_per_school + (i+1)
+        f = first_names.sample
+        l = last_names.sample
+
+        if s == 1 or s == 4
+            g = [9, 10, 11, 12].sample
+        elsif s == 2 or s == 5
+            g = [6, 7, 8].sample
+        else
+            g = [1, 2, 3, 4, 5].sample
+        end
+
+        student_data += "INSERT INTO students (id, first_name, last_name, school_id, grade_level) VALUES (#{student_id}, \"#{f}\", \"#{l}\", #{s}, #{g});\n"
+
+        class_ids = (((s-1)*teachers_per_school*classes_per_teacher+1)..(s*teachers_per_school*classes_per_teacher)).to_a.sample(classes_per_student)
+        class_ids.each do |class_id|
+            join_data += "INSERT INTO classes_students (class_id, student_id) VALUES (#{class_id}, #{student_id});\n"
+            tests_per_student_per_class.times do |j|
+                score = (60..100).to_a.sample
+                test_data += "INSERT INTO tests (name, score, class_id, student_id) VALUES (\"Test #{j+1}\", #{score}, #{class_id}, #{student_id});\n"
+            end
+        end
+    end
+end
+
+file.write("/* Teacher Data */\n")
+file.write(teacher_data)
+file.write("/* Class Data */\n")
+file.write(class_data)
+file.write("/* Student Data */\n")
+file.write(student_data)
+file.write("/* Class-Student Join Data */\n")
+file.write(join_data)
+file.write("/* Test Data */\n")
+file.write(test_data)
+
+
+file.close()
+
